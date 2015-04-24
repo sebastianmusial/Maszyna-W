7b72b2f63efaa45294867e8d7edfc03b357cb4e0
bsjhx <rakoczymichal@hotmail.com>
2015-04-24 15:34:31 +0200

    Addes date to query

--- database/queries.sql
+++ database/queries.sql
@@ -1,8 +1,8 @@
-select u.login 
+select u.login, r.date 
 from Topics t, Reply r, Users u
 where t.topicID = r.topicID and u.userID = r.userID and r.replyID = 
 ( 
 	select max(r1.replyID) 
-	from Reply r1, Topics t1
-	where r1.topicID = t1.topicID
+	from Reply r1
+	where r1.topicID = t.topicID
 );
